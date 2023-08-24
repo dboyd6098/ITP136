8748913555860cfa35662a963e4bc311b55a1889
dboyd6098 <dboyd6098@email.vccs.edu>
2023-08-24 09:27:37 -0400

    Finished

---
+++ Week1Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WeekOne
+{
+    using static System.Console;
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Who is being seen today?");
+            string patientName = ReadLine();
+            WriteLine("Welcome: " + patientName);
+            ReadKey();
+
+        }
+
+
+    }
+
+}
+
