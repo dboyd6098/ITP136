c5eda4eca687ef3b98b9a69eeb916f49c0b74a87
dboyd6098 <dboyd6098@email.vccs.edu>
2023-09-04 12:26:08 -0400

    Finished

---
+++ Week2Assignment/Week2Assignment/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace WeekTwo
+{
+    using static System.Console;
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            
+            int num1 = 5;
+            double num3 = 5.1;
+
+            string firstName = "Dave";
+            char middleInit = 'G';
+
+            int num4, num5, num6;
+
+            const string lastName = "Boyd";
+
+            double a, b, c;
+
+            double total;
+
+            string inputString;
+
+            Console.WriteLine("Enter your first number");
+            inputString = Console.ReadLine();
+
+            a = Convert.ToDouble(inputString);
+
+            Console.WriteLine("Enter your second number");
+            inputString = Console.ReadLine();
+
+            b = Convert.ToDouble(inputString);
+
+            Console.WriteLine("Enter your third number");
+            inputString = Console.ReadLine();
+
+            c = Convert.ToDouble(inputString);
+
+            total = (a + b + c) / 3;
+
+            Console.WriteLine("Your average is {0}", total.ToString("C"));
+
+            Console.WriteLine("Your numbers are {0,5},{1,5}{2,10}", a, b, c);
+
+            Console.ReadKey();
+
+
+        }
+
+
+    }
+
+}
+
---
+++ Week2Project/Week2Project/Week2Project/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Week2Project
+{
+    using static System.Console;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double oil, tires, inspection, subTotal, tax, grandTotal;
+            
+            string inputString;
+          
+
+            WriteLine("Welcome to the Auto Repair Calculator.");
+           
+            WriteLine("What is the cost for the oil change?");
+            inputString = Console.ReadLine();
+
+            oil = Convert.ToDouble(inputString);
+
+            WriteLine("What is the cost for tires?");
+            inputString = Console.ReadLine();
+
+            tires = Convert.ToDouble(inputString);
+
+            WriteLine("What is the cost for this inpsection?");
+            inputString = Console.ReadLine();
+
+            inspection = Convert.ToDouble(inputString);
+
+            subTotal = oil + tires + inspection;
+
+            WriteLine("The subtotal for this bill is {0}", subTotal.ToString("C"));
+
+            tax = subTotal * 6 / 100;
+
+            WriteLine("The tax for this bill is {0}", tax.ToString("C"));
+
+            grandTotal = subTotal + tax;
+
+            WriteLine("The grand total of this bill is {0}", grandTotal.ToString("C"));
+      
+
+        
+         
+        }
+    }
+}
